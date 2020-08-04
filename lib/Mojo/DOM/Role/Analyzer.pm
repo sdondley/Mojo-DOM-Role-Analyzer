bc540d0717067e3b208a166aa2b20ec9cf04fd3a
Steve Dondley <s@dondley.com>
2020-08-04 13:37:14 -0400

    init commit

--- lib/Mojo/DOM/Role/Analyzer.pm
+++ lib/Mojo/DOM/Role/Analyzer.pm
@@ -1,89 +1,163 @@
 package Mojo::DOM::Role::Analyzer ;
 
-use Moose;
-use namespace::autoclean;
-#use Moose::Role;
-#with qw( );
+use strict;
+use warnings;
+use Log::Log4perl::Shortcuts qw(:all);
+use Role::Tiny;
+use Carp;
 
-has '' => (is => '___', isa => '___', required => ___, lazy => ___, default => sub { ___ } );
-has '' => (is => '___', isa => '___', required => ___, lazy => ___, default => sub { ___ } );
-
-# methods here
-
-sub BUILD {
-
+sub element_count {
+  my $self = shift;
+  return $self->descendant_nodes->grep(sub { $_->type eq 'tag' })->size;
 }
 
-### Public methods ###
+sub parent_all {
+  my $self = shift;
+  my $tag = shift || $self->tag;
+  carp 'Unable to determine tag' if !$tag;
+  my $p_count = $self->root->find($tag)->size;
 
-sub  {
-  ___
+  my $enclosing_tag = $self->root->at($tag);
+  my $current_p_count = 0;
+  while ($current_p_count < $p_count) {
+    my $parent = $enclosing_tag->parent;
+    $current_p_count = $parent->find('p')->size;
+    $enclosing_tag = $parent;
+  }
+  return $enclosing_tag;
 }
 
-### Private methods ###
+sub parent_ptags {
+  my $self = shift;
+  return $self->parent_all('p');
+}
 
-sub  {
-  ___
+# determine if a tag A comes before or after tag B in the dom
+sub compare_tags {
+  my $s = shift;
+  my $tag1 = shift;
+  my $tag2 = shift;
+
+  my @t1_path = split / > /, $tag1->selector;
+  my @t2_path = split / > /, $tag2->selector;
+
+  foreach my $p1 (@t1_path) {
+    my $p2 = shift(@t2_path);
+    next if $p1 eq $p2;
+    my ($p1_tag, $p1_num) = split /:/, $p1;
+    my ($p2_tag, $p2_num) = split /:/, $p2;
+
+    next if $p1_num eq $p2_num;
+    return ($p1_num cmp $p2_num);
+  }
+}
+
+sub depth {
+  my $s = shift;
+  my $sel = $s->selector;
+  my @parts = split /\s>\s/, $sel;
+  return scalar @parts;
+}
+
+sub deepest {
+  my $s = shift;
+  my $deepest_depth = 0;
+  foreach my $c ($s->descendant_nodes->grep(sub { $_->type eq 'tag' })->each) {
+    my $depth = $c->depth;
+    $deepest_depth = $depth if $depth > $deepest_depth;
+  }
+  return $deepest_depth;
 }
 
 
-__PACKAGE__->meta->make_immutable;
 1; # Magic true value
-# ABSTRACT: this is what the module does
+# ABSTRACT: miscellaneous methods for analyzing a DOM
 
 __END__
 
 =head1 OVERVIEW
 
-Provide overview of who the intended audience is for the module and why it's useful.
+Provides methods for analyzing a DOM.
 
 =head1 SYNOPSIS
 
-  use Mojo::DOM::Role::Analyzer;
+  use strict;
+  use warnings;
+  use feature 'say';
+  use Mojo::Dom;
+
+  my $html = '<html><head></head><body><p class="first">A paragraph.</p><p class="last">boo<a>blah<span>kdj</span></a></p><h1>hi</h1></body></html>';
+  my $analyzer = Mojo::DOM->with_roles('+Analyzer')->new($html);
+
+  # return the count of elements inside a dom objec
+  my $count = $analyzer->at('body')->element_count;
+
+  # get the smallest containing dom object that contains all the paragraph tags
+  my $containing_dom = $analyzer->parent_ptags;
+
+  # compare DOM objects to see which comes first in the document
+  my $tag1 = $analyzer->at('p.first');
+  my $tag2 = $analyzer->at('p.last');
+  say $analyzer->compare_tags($tag1, $tag2);
+
+  # get the depth level of a dom object relative to root
+  # root node returns '1'
+  my $depth = $analyzer->at('p.first')->depth;
+
+  # get the deepest depth of the documented
+  my $deepest = $analyzer->deepest;
 
 =head1 DESCRIPTION
 
-=method method1()
+=h2 element_count
+
+  $count = $dom->element_count;
+
+Returns the number of elements in a dom oject, including children of children of children, etc.
 
 
+=h2 parent_all
 
-=method method2()
+  $dom = $dom->parent_all('a');                    # finds parent with root
+  $dom = $dom->at('div.article')->parent_all('a'); # finds parent within C<div.article>
 
-
-
-=func function1()
-
-
-
-=func function2()
+Returns the smallest containing $dom within the $dom the method is called on
+that wraps all the tags indicated in the argument.
 
 
+=h2 parent_ptags
 
-=attr attribute1
+  $dom = $dom->parent_ptags;
+  $dom = $dom->at('div.article')->parent_ptags;
+
+A conveniece method that works like the C<parent_all> method but automatically supplies a
+C<'p'> tag argument for you.
 
 
+=h2 compare_tags($dom1, $dom2)
 
-=attr attribute2
+  my $dom1 = $dom->at('p.first');
+  my $dom2 = $dom->at('p.last');
+  $dom->compare_tags($dom1, $dom2); # returns -1
+
+Compares the selectors of two $dom objects to see which comes first in the DOM.
+
+Returns -1 if the first argument comes before (is less than) the second.
+Returns 0 if the first and second arguments are the same.
+Returns 1 if the first argument comes after (is greater than) the second.
 
 
+=h2 depth
 
-#=head1 CONFIGURATION AND ENVIRONMENT
-#
-#Mojo::DOM::Role::Analyzer requires no configuration files or environment variables.
+  my $depth = $dom->at('p.first')->depth;
+
+Finds the nested depth level of a node. The root node returns 1.
+  
+=h2 deepest
+
+  my $deepest_depth = $dom->deepest;
+
+Finds the deeepest nested level within a node.
 
 
 =head1 DEPENDENCIES
-
-=head1 AUTHOR NOTES
-
-=head2 Development status
-
-This module is currently in the beta stages and is actively supported and maintained. Suggestion for improvement are welcome. 
-
-- Note possible future roadmap items.
-
-=head2 Motivation
-
-Provide motivation for writing the module here.
-
-#=head1 SEE ALSO
--- t/01-class.t
+++ t/01-class.t
@@ -1,24 +1,41 @@
-#/usr/bin/env perl
-use t::TestUtils;
-use Test::More;
-use Mojo::DOM::Role::Analyzer;
+use Test::Most;
+use Mojo::DOM;
 
-my $tests = 1; # keep on line 17 for ,i (increment and ,d (decrement)
+use strict;
+use warnings;
+
+
+
+
+
+
+
+
+
+
+
+my $tests = 5; # keep on line 17 for ,i (increment and ,d (decrement)
+plan tests => 5;
 diag( "Running my tests" );
 
-my @modules = qw/ Moose /;
-#my @methods = qw/ BUILD /;
-#my %attribs = (
-#   'attr'  => { type    => 'Str', lazy => 0, read => 'ro', req => 0,
-#                default => '', },
-#);
+my $html = '<html><head></head><body><p class="first">A paragraph.</p><p class="last">boo<a>blah<span>kdj</span></a></p><h1>hi</h1></body></html>';
 
-plan tests => $tests;
+my $ex = Mojo::DOM->with_roles('+Analyzer')->new($html);
 
-# class tests
-subtest 'module checks'   => \&module_check, @modules;
-#subtest 'attribute check' => \&attrib_check, ('Mojo::DOM::Role::Analyzer', \%attribs);
-#subtest 'method checks'   => \&method_check, ('Mojo::DOM::Role::Analyzer', @methods);
+my $count = $ex->at('body')->element_count;
+is $count, 5, 'gets element count';
 
-# create an object
-my $obj = Mojo::DOM::Role::Analyzer->new();
+my $tag = $ex->parent_ptags->tag;
+is $tag, 'body', 'gets correct container tag for paragraphs';
+
+my $tag1 = $ex->at('p.first');
+my $tag2 = $ex->at('p.last');
+
+my $result = $ex->compare_tags($tag1, $tag2);
+is $result, -1, 'can compare tags';
+
+my $depth = $ex->at('p.first')->depth;
+is $depth, 3, 'gets depth';
+
+my $deepest = $ex->deepest;
+is $deepest, 5, 'gets deepest depth';
